720ef25dbc5a0970ecc1c932c3793a59e25efed7
Letsibogo Ramadi <letsibogo.ramadi@citrix.com>
2016-08-24 15:46:34 +0100

    Added script header
    Description, Synopsis and some parameters
    
    Signed-off-by: Letsibogo Ramadi <letsibogo.ramadi@citrix.com>

---
+++ smbcluster.ps1
@@ -0,0 +1,14 @@
+﻿<#
+    .SYNOPSIS
+        Creates a Windows Server 2012R2 SMB3 failover cluster
+
+    .DESCRIPTION
+        This script automates the creation of a Windows Server 2012R2
+        failover cluster and configures it to use SMB3 storage
+
+    .PARAMETER XenServerHost
+        The XenServer host to connect to
+
+    .PARAMETER Nodes
+        The VM nodes used to form the cluster
+#>
